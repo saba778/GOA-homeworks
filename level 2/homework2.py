ecaffece97cc4ca332e73286d1356dc3942efaae
saba778 <mindiashvilisaba2912@gmail.com>
2025-05-06 18:36:30 +0400

    fourth commit

---
+++ level 2/homework2.py
@@ -0,0 +1,35 @@
+# 1 davaleba
+num1 = 10
+num2 = 9
+#mivumatot ertmanets
+print(num1 + num2)
+#gamovaklot
+print(num1 - num2)
+#gavamravlot
+print(num1 * num2)
+#gavyot
+print(num1 / num2)
+
+# 2 davaleba
+name = 'saba'
+print(name * 11)
+# saba daiwereba 11 jer
+
+# 3 davaleba
+surname = 'mindiashvili'
+print(name + ' ' +  surname)
+
+# 4 davaleba
+print((name + ' ' +  surname + ' ') * 100)
+
+# 5 davaleba
+#konkentinacia aris rodesac ramodenime cvladebs ertmanetan aerteb magalitad
+l = 'basket'
+b = 'ball'
+print(l + b)
+
+# 6 davaleba
+#city => "London'
+#print"city)
+city = 'London'
+print(city)
---
+++ level 2/lesson.py
@@ -0,0 +1,24 @@
+# ცვლადებს ვიყენებთ რომ რაიმე დავიმახსოვროთ
+number1 = 20
+number2 = 4
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+
+
+
+
+surname = "mindiashvili"
+age = 14
+print(surname * age)
+
+
+
+
+
+name = "saba."
+surname1 = "mindiashvili."
+age1 = "14."
+hight = "170 cm." 
+print("hello my name is" + " " +  name  + " " + "my surname is" + " " + surname1 + " " "my age is" + " " + age1 + " " "my hight is" + " " + hight)
