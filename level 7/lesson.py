3c0622abf5ad18bdc241daa7c882398ccc8248a6
saba778 <mindiashvilisaba2912@gmail.com>
2025-06-11 13:55:14 +0400

    fith commit

---
+++ level 7/lesson.py
@@ -0,0 +1,21 @@
+#1)
+num1 = 20
+num2 = 14
+sum = num1 + num2
+print(sum)
+#2)
+price = 50
+price = 80
+print(price)
+#3)
+name = input('enter your name user:')
+age = input('enter your age here user:')
+print("your name is" + ' ' + name + ' ' "your age is" + ' ' + age) 
+#4)
+#debugging არის შენ მიერ შექმნილი კოდის შეცდომების დანახვა და შესწორება
+#5)
+#კომენტარის უკეთესად შეგვიძლია სხვას პროგრამერს გავაგებინოთ შენ მიერ დაწერილი კოდი, და თუ ბევრი 
+# ხნის შემდეგ დაუბრუნდები შენ კოდა შეგიძლია გაიხსენო რას აკეთებდი
+#6)
+number = 11
+Number = 11
